ae73bb686845831aff2ea28d1379e6bed17ea853
奇卡之家 <1828665870@qq.com>
2026-06-14 14:50:26 +0800

    dog golem armor

--- src/main/java/golemknights/tinkersgolem/register/TGItems.java
+++ src/main/java/golemknights/tinkersgolem/register/TGItems.java
@@ -3,9 +3,12 @@ package golemknights.tinkersgolem.register;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
 import slimeknights.mantle.registration.object.EnumObject;
+import slimeknights.mantle.registration.object.ItemObject;
 import slimeknights.tconstruct.common.Sounds;
 import slimeknights.tconstruct.library.tools.definition.ModifiableArmorMaterial;
+import golemknights.tinkersgolem.item.armor.ModifiableDogGolemArmorItem;
 import golemknights.tinkersgolem.item.armor.ModifiableMetalGolemArmorItem;
 import static golemknights.tinkersgolem.TinkersGolem.ITEMS;
 import static golemknights.tinkersgolem.TinkersGolem.getResource;
@@ -14,18 +17,26 @@ import static dev.xkmc.modulargolems.content.client.armor.GolemModelPaths.HELMET
 import static dev.xkmc.modulargolems.content.client.armor.GolemModelPaths.CHESTPLATES;
 import static dev.xkmc.modulargolems.content.client.armor.GolemModelPaths.LEGGINGS;
 import static dev.xkmc.modulargolems.content.client.armor.GolemModelPaths.BOOTS_DIAMOND;
+
 public class TGItems {
-        public static void load() {
-        }
-        public static final String METAL_GOLEM = "metal_golem";
-        public static final ModifiableArmorMaterial GOLEM = ModifiableArmorMaterial.create(getResource(METAL_GOLEM), Sounds.EQUIP_PLATE.getSound());
-        protected static final Item.Properties UNSTACKABLE_PROPS = new Item.Properties().stacksTo(1);
-        public static final ResourceLocation[] METAL_GOLEM_ARMOR_MODELS = { HELMETS, CHESTPLATES, LEGGINGS, BOOTS_DIAMOND };
-        public static final EnumObject<ArmorItem.Type, ModifiableMetalGolemArmorItem> metalGolemArmor = ITEMS
-                        .registerEnum(
-                                        METAL_GOLEM, ArmorItem.Type.values(),
-                                        type -> new ModifiableMetalGolemArmorItem(UNSTACKABLE_PROPS, type,
-                                                        GOLEM,
-                                                        METAL_GOLEM_ARMOR_MODELS[type.ordinal()], getResource(METAL_GOLEM+"_"+type.getName())));
-        
+    public static void load() {
+    }
+    public static final String METAL_GOLEM = "metal_golem";
+    public static final ModifiableArmorMaterial GOLEM = ModifiableArmorMaterial.create(getResource(METAL_GOLEM),
+            Sounds.EQUIP_PLATE.getSound());
+    protected static final Item.Properties UNSTACKABLE_PROPS = new Item.Properties().stacksTo(1);
+    public static final ResourceLocation[] METAL_GOLEM_ARMOR_MODELS = { HELMETS, CHESTPLATES, LEGGINGS,
+            BOOTS_DIAMOND };
+    public static final EnumObject<ArmorItem.Type, ModifiableMetalGolemArmorItem> metalGolemArmor = ITEMS
+            .registerEnum(
+                    METAL_GOLEM, ArmorItem.Type.values(),
+                    type -> new ModifiableMetalGolemArmorItem(UNSTACKABLE_PROPS, type,
+                            GOLEM,
+                            METAL_GOLEM_ARMOR_MODELS[type.ordinal()],
+                            getResource(METAL_GOLEM + "_" + type.getName())));
+
+    public static final String DOG_GOLEM_ARMOR = "dog_golem_armor";
+    public static final ItemObject<ModifiableDogGolemArmorItem> dogGolemArmor = ITEMS.register(
+            DOG_GOLEM_ARMOR,
+            () -> new ModifiableDogGolemArmorItem(UNSTACKABLE_PROPS, GOLEM, getResource(DOG_GOLEM_ARMOR)));
 }
