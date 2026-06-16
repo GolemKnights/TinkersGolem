f3775c06ced125c46d6d9d04cad8b4a6e36d8ada
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-16 08:22:31 +0800

    item rendering transform

--- src/main/java/golemknights/tinkersgolem/entity/SlimeGolemModel.java
+++ src/main/java/golemknights/tinkersgolem/entity/SlimeGolemModel.java
@@ -4,24 +4,36 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import dev.xkmc.modulargolems.content.entity.common.IGolemModel;
 import dev.xkmc.modulargolems.content.entity.common.IHeadedModel;
-import net.minecraft.client.model.SlimeModel;
+import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class SlimeGolemModel extends SlimeModel<SlimeGolemEntity> implements IGolemModel<SlimeGolemEntity, SlimeGolemPartType, SlimeGolemModel>, IHeadedModel {
+public class SlimeGolemModel extends HierarchicalModel<SlimeGolemEntity> implements IGolemModel<SlimeGolemEntity, SlimeGolemPartType, SlimeGolemModel>, IHeadedModel {
 
+	private final ModelPart root;
 	private final ModelPart outer;
 
 	public SlimeGolemModel(EntityModelSet set) {
-		super(set.bakeLayer(ModelLayers.SLIME));
+		super(RenderType::entityTranslucent);
+		root = set.bakeLayer(ModelLayers.SLIME);
 		outer = set.bakeLayer(ModelLayers.SLIME_OUTER);
 	}
 
+	@Override
+	public ModelPart root() {
+		return root;
+	}
+
+	@Override
+	public void setupAnim(SlimeGolemEntity e, float p_102619_, float p_102620_, float p_102621_, float p_102622_, float p_102623_) {
+
+	}
 
 	@Override
 	public void renderToBufferInternal(SlimeGolemPartType type, PoseStack stack, VertexConsumer consumer, int i, int j, float f1, float f2, float f3, float f4) {
--- src/main/java/golemknights/tinkersgolem/entity/SlimeGolemPartType.java
+++ src/main/java/golemknights/tinkersgolem/entity/SlimeGolemPartType.java
@@ -1,7 +1,9 @@
 package golemknights.tinkersgolem.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import dev.xkmc.modulargolems.content.core.IGolemPart;
+import dev.xkmc.modulargolems.content.entity.dog.DogGolemPartType;
 import dev.xkmc.modulargolems.content.item.golem.GolemPart;
 import golemknights.tinkersgolem.register.TGEntities;
 import net.minecraft.ChatFormatting;
@@ -31,7 +33,56 @@ public enum SlimeGolemPartType implements IGolemPart<SlimeGolemPartType> {
 
 	@Override
 	public void setupItemRender(PoseStack stack, ItemDisplayContext transform, @Nullable SlimeGolemPartType part) {
+		//TODO 如下渲染从狗傀儡渲染复制。还需调整渲染角度和偏移
 
+		switch (transform) {
+			case GUI:
+			case FIRST_PERSON_LEFT_HAND:
+			case FIRST_PERSON_RIGHT_HAND:
+				break;
+			case THIRD_PERSON_LEFT_HAND:
+			case THIRD_PERSON_RIGHT_HAND: {
+				stack.translate(0.25, 0.4, 0.5);
+				float size = 0.5F;
+				stack.scale(size, size, size);
+				break;
+			}
+			case GROUND: {
+				stack.translate(0.25, 0, 0.5);
+				float size = 0.5F;
+				stack.scale(size, size, size);
+				break;
+			}
+			case NONE:
+			case HEAD:
+			case FIXED: {
+				stack.translate(0.5, 0.5, 0.5);
+				float size = 1f;
+				stack.scale(size, -size, size);
+				stack.translate(0, -0.5, 0);
+				return;
+			}
+			default:
+				stack.translate(0.1, 0, 0.5);
+				float size = 0.75F;
+				stack.scale(size, size, size);
+				break;
+		}
+		stack.mulPose(Axis.ZP.rotationDegrees(135));
+		stack.mulPose(Axis.YP.rotationDegrees(-155));
+		if (part == null) {
+			float size = 1f;
+			stack.scale(size, size, size);
+			stack.translate(0, -1.9, 0);
+		} else if (part == INNER) {
+			float size = 1f;
+			stack.scale(size, size, size);
+			stack.translate(0, -1.9, 0);
+		} else if (part == OUTER) {
+			float size = 1f;
+			stack.scale(size, size, size);
+			stack.translate(0, -1.9, 0);
+		}
 	}
 
 }
