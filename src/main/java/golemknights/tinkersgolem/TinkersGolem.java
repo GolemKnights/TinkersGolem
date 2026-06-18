45c85da3e0057a86b5b96e145710389d2e6c9a6a
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-18 22:18:00 +0800

    fix fluid sync

--- src/main/java/golemknights/tinkersgolem/TinkersGolem.java
+++ src/main/java/golemknights/tinkersgolem/TinkersGolem.java
@@ -10,6 +10,7 @@ import golemknights.tinkersgolem.data.TGConfigGen;
 import golemknights.tinkersgolem.data.TGLang;
 import golemknights.tinkersgolem.data.TGRecipeGen;
 import golemknights.tinkersgolem.data.TGTagGen;
+import golemknights.tinkersgolem.entity.SlimeTankSyncPacket;
 import golemknights.tinkersgolem.events.TGAttackListener;
 import golemknights.tinkersgolem.register.*;
 import net.minecraft.core.particles.ParticleType;
@@ -42,83 +43,84 @@ import slimeknights.mantle.registration.deferred.SynchronizedDeferredRegister;
 @SuppressWarnings("removal")
 public class TinkersGolem {
 
-    public static final String MODID = "tinkers_golem";
-    public static final Logger LOGGER = LogManager.getLogger();
-    public static final L2Registrate REGISTRATE = new L2Registrate(MODID);
-    public static final IEventBus MOD_BUS = FMLJavaModLoadingContext.get().getModEventBus();
+	public static final String MODID = "tinkers_golem";
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final L2Registrate REGISTRATE = new L2Registrate(MODID);
+	public static final IEventBus MOD_BUS = FMLJavaModLoadingContext.get().getModEventBus();
 
-    public static final PacketHandlerWithConfig HANDLER = new PacketHandlerWithConfig(
-            getResource("main"), 1,
-            e -> e.create(OverslimeSyncPacket.class, NetworkDirection.PLAY_TO_CLIENT)
-    );
+	public static final PacketHandlerWithConfig HANDLER = new PacketHandlerWithConfig(
+			getResource("main"), 1,
+			e -> e.create(OverslimeSyncPacket.class, NetworkDirection.PLAY_TO_CLIENT),
+			e -> e.create(SlimeTankSyncPacket.class, NetworkDirection.PLAY_TO_CLIENT)
+	);
 
-    public static final ItemDeferredRegister ITEMS = new ItemDeferredRegister(MODID);
-    public static final EntityTypeDeferredRegister ENTITIES = new EntityTypeDeferredRegister(MODID);
-    public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, MODID);
-    public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Registries.RECIPE_TYPE, MODID);
-    public static final SynchronizedDeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = SynchronizedDeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, MODID);
-    public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, MODID);
-    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-    // public static final ConfigTypeEntry<SpawnConfig> SPAWN = new
-    // ConfigTypeEntry<>(HANDLER, "spawn", SpawnConfig.class);
-    // public static final ConfigTypeEntry<EquipmentConfig> ITEMS = new
-    // ConfigTypeEntry<>(HANDLER, "equipment", EquipmentConfig.class);
-    // public static final ConfigTypeEntry<TrialConfig> TRIAL = new
-    // ConfigTypeEntry<>(HANDLER, "trial", TrialConfig.class);
+	public static final ItemDeferredRegister ITEMS = new ItemDeferredRegister(MODID);
+	public static final EntityTypeDeferredRegister ENTITIES = new EntityTypeDeferredRegister(MODID);
+	public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, MODID);
+	public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Registries.RECIPE_TYPE, MODID);
+	public static final SynchronizedDeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = SynchronizedDeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, MODID);
+	public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, MODID);
+	public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+	// public static final ConfigTypeEntry<SpawnConfig> SPAWN = new
+	// ConfigTypeEntry<>(HANDLER, "spawn", SpawnConfig.class);
+	// public static final ConfigTypeEntry<EquipmentConfig> ITEMS = new
+	// ConfigTypeEntry<>(HANDLER, "equipment", EquipmentConfig.class);
+	// public static final ConfigTypeEntry<TrialConfig> TRIAL = new
+	// ConfigTypeEntry<>(HANDLER, "trial", TrialConfig.class);
 
-    public TinkersGolem() {
-        TGItems.load();
-        TGAttributes.load();
-        TGEntities.load();
-        TGParticles.load();
-        TGGolemModifiers.load();
-        TGRecipes.load();
-        MinecraftForge.EVENT_BUS.register(this);
-        ITEMS.register(MOD_BUS);
-        ENTITIES.register(MOD_BUS);
-        PARTICLES.register(MOD_BUS);
-        RECIPE_TYPES.register(MOD_BUS);
-        RECIPE_SERIALIZERS.register(MOD_BUS);
-        ATTRIBUTES.register(MOD_BUS);
-        TABS.register(MOD_BUS);
-        TGTinkersModifiers.registers(MOD_BUS);
-        MOD_BUS.addListener(TGAttributes::setupAttributes);
-        // GDItems.register();
-        // GDModifiers.register();
-        // GDWorldGen.register();
-        // GDTriggers.register();
-        // GDConfig.init();
-        OverslimeCap.register();
-        AttackEventHandler.register(3516, new TGAttackListener());
-    }
+	public TinkersGolem() {
+		TGItems.load();
+		TGAttributes.load();
+		TGEntities.load();
+		TGParticles.load();
+		TGGolemModifiers.load();
+		TGRecipes.load();
+		MinecraftForge.EVENT_BUS.register(this);
+		ITEMS.register(MOD_BUS);
+		ENTITIES.register(MOD_BUS);
+		PARTICLES.register(MOD_BUS);
+		RECIPE_TYPES.register(MOD_BUS);
+		RECIPE_SERIALIZERS.register(MOD_BUS);
+		ATTRIBUTES.register(MOD_BUS);
+		TABS.register(MOD_BUS);
+		TGTinkersModifiers.registers(MOD_BUS);
+		MOD_BUS.addListener(TGAttributes::setupAttributes);
+		// GDItems.register();
+		// GDModifiers.register();
+		// GDWorldGen.register();
+		// GDTriggers.register();
+		// GDConfig.init();
+		OverslimeCap.register();
+		AttackEventHandler.register(3516, new TGAttackListener());
+	}
 
-    @SubscribeEvent
-    public static void modifyAttributes(EntityAttributeModificationEvent event) {
-    }
+	@SubscribeEvent
+	public static void modifyAttributes(EntityAttributeModificationEvent event) {
+	}
 
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public static void gatherData(GatherDataEvent event) {
-        REGISTRATE.addDataGenerator(ProviderType.LANG, TGLang::genLang);
-        // REGISTRATE.addDataGenerator(ProviderType.LOOT, GDLootGen::genLoot);
-        REGISTRATE.addDataGenerator(ProviderType.RECIPE, TGRecipeGen::genRecipe);
-        // REGISTRATE.addDataGenerator(ProviderType.ADVANCEMENT, GDAdvGen::genAdv);
-        REGISTRATE.addDataGenerator(ProviderType.ITEM_TAGS, TGTagGen::genItemTag);
+	@SubscribeEvent(priority = EventPriority.HIGH)
+	public static void gatherData(GatherDataEvent event) {
+		REGISTRATE.addDataGenerator(ProviderType.LANG, TGLang::genLang);
+		// REGISTRATE.addDataGenerator(ProviderType.LOOT, GDLootGen::genLoot);
+		REGISTRATE.addDataGenerator(ProviderType.RECIPE, TGRecipeGen::genRecipe);
+		// REGISTRATE.addDataGenerator(ProviderType.ADVANCEMENT, GDAdvGen::genAdv);
+		REGISTRATE.addDataGenerator(ProviderType.ITEM_TAGS, TGTagGen::genItemTag);
 
-        var gen = event.getGenerator();
-        var output = gen.getPackOutput();
-        var pvd = event.getLookupProvider();
-        var helper = event.getExistingFileHelper();
-        var server = event.includeServer();
-        gen.addProvider(server, new TGConfigGen(gen));
-    }
+		var gen = event.getGenerator();
+		var output = gen.getPackOutput();
+		var pvd = event.getLookupProvider();
+		var helper = event.getExistingFileHelper();
+		var server = event.includeServer();
+		gen.addProvider(server, new TGConfigGen(gen));
+	}
 
-    /**
-     * Get the ResourceLocation in Tinkers' Golem's namespace.
-     *
-     * @return the ResourceLocation.
-     */
-    public static ResourceLocation getResource(String id) {
-        return new ResourceLocation(MODID, id);
-    }
+	/**
+	 * Get the ResourceLocation in Tinkers' Golem's namespace.
+	 *
+	 * @return the ResourceLocation.
+	 */
+	public static ResourceLocation getResource(String id) {
+		return new ResourceLocation(MODID, id);
+	}
 
 }
