c07497bd3b0f1e8eae643ed820e5e86424ccd6af
CreepingCreeper <2714328217@qq.com>
2026-06-18 19:28:21 +0800

    黏液覆层小图标

--- src/main/java/golemknights/tinkersgolem/compat/OverslimeProvider.java
+++ src/main/java/golemknights/tinkersgolem/compat/OverslimeProvider.java
@@ -6,10 +6,15 @@ import golemknights.tinkersgolem.cap.OverslimeCap;
 import golemknights.tinkersgolem.data.TGLang;
 import golemknights.tinkersgolem.register.TGAttributes;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.phys.Vec2;
 import snownee.jade.api.EntityAccessor;
 import snownee.jade.api.IEntityComponentProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.IElement;
+import snownee.jade.api.ui.IElementHelper;
 
 public enum OverslimeProvider implements IEntityComponentProvider {
 
@@ -20,11 +25,14 @@ public enum OverslimeProvider implements IEntityComponentProvider {
 	@Override
 	public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig config) {
 		if (accessor.getEntity() instanceof AbstractGolemEntity<?, ?> golem) {
-			var attr = golem.getAttribute(TGAttributes.MAX_OVERSLIME.get());
+            var attr = golem.getAttribute(TGAttributes.MAX_OVERSLIME.get());
 			if (attr == null || attr.getValue() == 0) return;
+            IElementHelper elements = IElementHelper.get();
+            IElement icon = elements.smallItem(new ItemStack(Items.SLIME_BALL));
+            tooltip.add(icon.translate(new Vec2(-0.75F, 0)));
 			int max = (int) attr.getValue();
 			var val = (int) OverslimeCap.HOLDER.get(golem).overslime;
-			tooltip.add(TGLang.OVERSLIME_INFO.get(val, max));
+			tooltip.append(TGLang.OVERSLIME_INFO.get(val, max));
 		}
 	}
 
