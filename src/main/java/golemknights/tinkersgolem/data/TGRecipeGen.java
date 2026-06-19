4a260e158116a7bc218080fc3b42addcda1320ad
CreepingCreeper <2714328217@qq.com>
2026-06-19 13:24:24 +0800

    史莱姆傀儡合成配方新增对称版本

--- src/main/java/golemknights/tinkersgolem/data/TGRecipeGen.java
+++ src/main/java/golemknights/tinkersgolem/data/TGRecipeGen.java
@@ -60,6 +60,13 @@ public class TGRecipeGen {
 					.define('B', TGEntities.SLIME_SHELL.get())
 					.save(pvd, TinkersGolem.getResource("slime_golem/assemble_holder"));
 
+            unlock(pvd, new GolemAssembleBuilder(TGEntities.HOLDER_SLIME.get(), 1)::unlockedBy,
+                    TGEntities.SLIME_CORE.get())
+                    .pattern("A").pattern("B")
+                    .define('A', TGEntities.SLIME_SHELL.get())
+                    .define('B', TGEntities.SLIME_CORE.get())
+                    .save(pvd, TinkersGolem.getResource("slime_golem/assemble_holder_inverted"));
+
 			unlock(pvd, new GolemReplaceBuilder(TGEntities.HOLDER_SLIME.get(), 1)::unlockedBy,
 					TGEntities.SLIME_CORE.get())
 					.pattern("P").pattern("H")
